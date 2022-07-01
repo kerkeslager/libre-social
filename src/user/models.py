f9f15a41379c831d5a32a33bfdcc2d8af198c44c
David Kerkeslager <kerkeslager@gmail.com>
2022-07-01 13:49:25 -0400

    Disply connections

--- src/user/models.py
+++ src/user/models.py
@@ -33,3 +33,49 @@ def save_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         instance.profile.save()
+
+@receiver(signals.post_save, sender=User)
+def create_circles(sender, instance, created, **kwargs):
+    if created:
+        Circle.objects.create(
+            owner=instance,
+            name='Family',
+            color='0000bb',
+        )
+        Circle.objects.create(
+            owner=instance,
+            name='Friends',
+            color='bb0000',
+        )
+
+
+class Circle(models.Model):
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False,
+    )
+    name = models.CharField(max_length = 256)
+    color = models.CharField(max_length = 6)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='circles',
+    )
+
+class Connection(models.Model):
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False,
+    )
+    circle = models.ForeignKey(
+        Circle,
+        on_delete=models.CASCADE,
+        related_name='connections',
+    )
+    connection = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reversed_connections',
+    )
--- src/user/serializers.py
+++ src/user/serializers.py
@@ -2,6 +2,24 @@ from rest_framework import exceptions, serializers
 
 from . import models
 
+class CircleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Circle
+        fields = (
+            'name',
+            'color',
+        )
+
+class ConnectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Connection
+        fields = (
+            'circle',
+        )
+
+    circle = CircleSerializer()
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Profile
@@ -9,9 +27,19 @@ class ProfileSerializer(serializers.ModelSerializer):
             'name',
             'description',
             'is_current_user',
+            'connections',
         )
 
     is_current_user = serializers.SerializerMethodField()
+    connections = serializers.SerializerMethodField()
 
     def get_is_current_user(self, obj):
         return self.context['request'].user.profile == obj
+
+    def get_connections(self, obj):
+        connections = obj.user.reversed_connections.filter(
+            circle__owner=self.context['request'].user
+        )
+        return [
+            ConnectionSerializer(c).data['circle'] for c in connections
+        ]
