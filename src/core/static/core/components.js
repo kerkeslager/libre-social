8ff6ea5ac7b72578a2180757f56ee225fda12fe3
David Kerkeslager <david@kerkeslager.com>
2023-04-10 17:05:51 -0400

    Massive push to get rid of React

--- src/core/static/core/components.js
+++ src/core/static/core/components.js
@@ -55,6 +55,12 @@ class Component extends HTMLElement {
   constructor() {
     super();
     this.state = {};
+
+    /*
+     * Put applyRender in the event loop, which should result in it being
+     * called after the element has been rendered completely.
+     */
+    setTimeout(() => this.applyRender());
   }
 
   applyRender() {
@@ -73,16 +79,16 @@ class Component extends HTMLElement {
     }
   }
 
-  connectedCallback() {
-    this.applyRender();
-  }
-
   setState(newState) {
     Object.keys(newState).forEach(k => {
       this.state[k] = newState[k];
     });
 
     this.applyRender();
+  }
+
+  render() {
+    return null;
   }
 }
 
@@ -99,18 +105,34 @@ class APIObjectComponent extends Component {
   }
 }
 
+class CircleIcon extends Component { }
+
+class CommonmarkRenderer extends Component {
+  render() {
+    let markup = this.getAttribute('commonmark');
+
+    let parser = new commonmark.Parser({ smart: true });
+    let writer = new commonmark.HtmlRenderer({ safe: true });
+
+    let parsed = parser.parse(markup);
+    return writer.render(parsed);
+  }
+}
+
 class CircleView extends APIObjectComponent {
   render() {
-    if(this.state.object === null) return '';
+    if(this.state.object === null) return null;
 
+    let circle = document.createElement('circle-icon');
+    circle.style.borderColor = '#' + this.state.object.color;
 
-    let editableHeader = document.createElement('editable-header');
-    editableHeader.setAttribute('depth', 1);
-    editableHeader.setAttribute('maxLength', 256);
-    editableHeader.setAttribute('value', this.state.object.name);
-    editableHeader.setAttribute('onSave', value => API.post(
+    let heading = document.createElement('editable-h1');
+    heading.setAttribute('depth', 1);
+    heading.setAttribute('maxLength', 256);
+    heading.setAttribute('value', this.state.object.name);
+    heading.addEventListener('save', e => API.post(
       this.getAttribute('path'),
-      { name: value },
+      { name: e.detail.value },
       {
         200: (status, data) => {
           this.setState({ object: data });
@@ -118,11 +140,49 @@ class CircleView extends APIObjectComponent {
       }
     ));
 
-    return [editableHeader];
+    let header = document.createElement('header');
+    header.appendChild(circle);
+    header.appendChild(heading);
+
+    return [header];
   }
 }
 
-class EditableHeader extends Component {
+class ProfileView extends APIObjectComponent {
+  render() {
+    if(this.state.object === null) return null;
+
+    let heading = document.createElement('editable-h1');
+    heading.setAttribute('maxLength', 256);
+    heading.setAttribute('value', this.state.object.name);
+    heading.addEventListener('save', e => API.post(
+      this.getAttribute('path'),
+      { name: e.detail.value },
+      {
+        200: (status, data) => {
+          this.setState({ object: data });
+        },
+      }
+    ));
+
+    let description = document.createElement('editable-commonmark-area');
+    description.setAttribute('maxLength', 4096);
+    description.setAttribute('commonmark', this.state.object.description);
+    description.addEventListener('save', e => API.post(
+      this.getAttribute('path'),
+      { description: e.detail.commonmark },
+      {
+        200: (status, data) => {
+          this.setState({ object: data });
+        },
+      }
+    ));
+
+    return [heading, description];
+  }
+}
+
+class EditableCommonmarkArea extends Component {
   constructor() {
     super();
 
@@ -132,14 +192,26 @@ class EditableHeader extends Component {
   }
 
   static get observedAttributes() {
-    return ['depth', 'maxLength', 'onSave', 'value'];
+    return ['maxLength', 'value'];
   }
 
   render() {
     if(this.state.editing) {
-      let input = document.createElement('input');
-      input.setAttribute('type', 'text');
-      input.setAttribute('value', this.getAttribute('value'));
+      let input = document.createElement('textarea');
+      input.value = this.getAttribute('commonmark');
+      input.addEventListener('input', () => {
+        input.style.height = 0;
+        input.style.height = `calc(${ input.scrollHeight }px - 1rem)`;
+      });
+
+      setTimeout(() => {
+        input.focus();
+        input.select();
+      });
+
+      let characterCounter = document.createElement('character-counter');
+      characterCounter.setAttribute('maxLength', this.getAttribute('maxLength'));
+      characterCounter.track(input);
 
       let submit = document.createElement('input');
       submit.setAttribute('type', 'submit');
@@ -148,13 +220,111 @@ class EditableHeader extends Component {
       let result = document.createElement('form');
       result.addEventListener('submit', e => {
         e.preventDefault();
+        this.dispatchEvent(new CustomEvent(
+          'save', {
+            detail: {
+              commonmark: input.value,
+            }
+          }
+        ));
         this.setState({ editing: false });
       });
       result.appendChild(input);
+      result.appendChild(characterCounter);
       result.appendChild(submit);
       return result;
     } else {
-      let h = document.createElement(`h${ this.getAttribute('depth') }`);
+      let h = document.createElement('commonmark-renderer');
+      h.setAttribute('commonmark', this.getAttribute('commonmark'));
+
+      let editButton = document.createElement('a');
+      editButton.setAttribute('href', '');
+      editButton.setAttribute('class', 'edit');
+      editButton.addEventListener('click', e => {
+        e.preventDefault();
+        this.setState({ editing: true });
+      });
+      editButton.textContent = 'Edit';
+
+      return [h, editButton];
+    }
+  }
+}
+
+class CharacterCounter extends Component {
+  track(input) {
+    this.setState({ characters: input.value.length });
+
+    input.addEventListener('input', () => this.setState({
+      characters: input.value.length
+    }));
+  }
+
+  render() {
+    let characters = this.state.characters;
+    let maxLength = this.getAttribute('maxLength');
+
+    if(characters > maxLength) {
+      this.style.color = '#cc0000';
+    } else if(characters > maxLength * 0.9) {
+      this.style.color = '#ddbb00';
+    } else {
+      this.style.color = 'black';
+    }
+
+    return `${ characters }/${ maxLength }`;
+  }
+}
+
+class EditableHeading extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      editing: false,
+    };
+  }
+
+  static get observedAttributes() {
+    return ['maxLength', 'value'];
+  }
+
+  render() {
+    if(this.state.editing) {
+      let input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      input.setAttribute('value', this.getAttribute('value'));
+      setTimeout(() => {
+        input.focus();
+        input.select();
+      });
+
+      let characterCounter = document.createElement('character-counter');
+      characterCounter.setAttribute('maxLength', this.getAttribute('maxLength'));
+      characterCounter.track(input);
+
+      let submit = document.createElement('input');
+      submit.setAttribute('type', 'submit');
+      submit.setAttribute('value', 'Save');
+
+      let result = document.createElement('form');
+      result.addEventListener('submit', e => {
+        e.preventDefault();
+        this.dispatchEvent(new CustomEvent(
+          'save', {
+            detail: {
+              value: input.value,
+            }
+          }
+        ));
+        this.setState({ editing: false });
+      });
+      result.appendChild(input);
+      result.appendChild(characterCounter);
+      result.appendChild(submit);
+      return result;
+    } else {
+      let h = document.createElement(`h${ this.depth }`);
       h.textContent = this.getAttribute('value');
 
       let editButton = document.createElement('a');
@@ -171,5 +341,46 @@ class EditableHeader extends Component {
   }
 }
 
+class EditableH1 extends EditableHeading {
+  constructor() {
+    super();
+    this.depth = 1;
+  }
+}
+class EditableH2 extends EditableHeading {
+  constructor() {
+    super();
+    this.depth = 2;
+  }
+}
+class EditableH3 extends EditableHeading {
+  constructor() {
+    super();
+    this.depth = 3;
+  }
+}
+class EditableH4 extends EditableHeading {
+  constructor() {
+    super();
+    this.depth = 4;
+  }
+}
+class EditableH5 extends EditableHeading {
+  constructor() {
+    super();
+    this.depth = 5;
+  }
+}
+
+customElements.define('circle-icon', CircleIcon);
+customElements.define('character-counter', CharacterCounter);
+customElements.define('commonmark-renderer', CommonmarkRenderer);
+customElements.define('editable-commonmark-area', EditableCommonmarkArea);
+customElements.define('editable-h1', EditableH1);
+customElements.define('editable-h2', EditableH2);
+customElements.define('editable-h3', EditableH3);
+customElements.define('editable-h4', EditableH4);
+customElements.define('editable-h5', EditableH5);
+
 customElements.define('circle-view', CircleView);
-customElements.define('editable-header', EditableHeader);
+customElements.define('profile-view', ProfileView);
--- src/user/api_urls.py
+++ src/user/api_urls.py
@@ -5,7 +5,7 @@ from . import api_views as views
 app_name = 'api'
 
 urlpatterns = [
-    path('user/<uuid:pk>', views.profile_detail_view, name='user'),
+    path('user/<uuid:pk>', views.profile_detail_view, name='profile'),
     path('user/me/delete', views.delete_view, name='delete-user'),
     path('circle/<uuid:pk>', views.circle_view, name='circle'),
 ]
